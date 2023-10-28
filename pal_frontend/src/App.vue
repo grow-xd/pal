1af1351a8d761eb2f08d0fe611317c42c6e9bda1
grow-xd <gupta.rg.rishabh@gmail.com>
2023-10-28 19:57:34 +0530

    Added the Login and SignUp pages and linked them to the backend and database

--- pal_frontend/src/App.vue
+++ pal_frontend/src/App.vue
@@ -1,55 +1,98 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
 <template>
   <!-- component -->
-  <div>
-    <div class="sticky top-0 z-50">
-      <div>
-        <div class="w-full bg-black opacity-80 h-20 flex justify-between">
-          <div class="w-full lg:w-30/6 xl:w-full h-full flex items-center px-4">
-            <img
-              class="rounded-lg bg-white w-32"
-              src="../logo.png"
-              alt="addify logo"
-            />
-          </div>
 
-          <div class="w-full h-full flex justify-end items-center">
-            <button class="bg-black text-white rounded-xl hover:bg-gray-800 p-2 m-2 mr-7">SignIn</button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <nav
+            v-if="isLoggedIn" class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
+            <RouterLink to="home"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                <!-- HeroIcon - User -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 shrink-0">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
 
-    <div class="text-7xl text-center">Pal app</div>
+                <small class="text-center text-xs font-medium"> Profile </small>
+            </RouterLink>
 
-    <aside
-      class="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16"
-    >
-      <div
-        class="relative z-10 max-w-screen-xl px-4 pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8"
-      >
-        <div
-          class="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto"
-        >
-          <h2 class="text-4xl font-bold sm:text-5xl">
-            Lorem Ipsum
-            <span class="hidden sm:block text-4xl"> Lorem Ipsum </span>
-          </h2>
+            <RouterLink to="friends"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+                <!-- HeroIcon - Chart Bar -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 shrink-0">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+                </svg>
 
-          <a
-            class="inline-flex text-white items-center px-6 py-3 font-medium bg-rose-500 rounded-lg hover:opacity-75"
-            href=""
-          >
-            
-            
-            &nbsp; Register
-          </a>
-        </div>
-      </div>
+                <small class="text-center text-xs font-medium"> Friends </small>
+            </RouterLink>
 
-      <div class="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full">
-        <img class="w-2/3" src="../friends.png" />
-      </div>
-    </aside>
+            <RouterLink to="Communities"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+                <!-- HeroIcon - Cog-6-tooth -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 shrink-0">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+
+                <small class="text-center text-xs font-medium"> Settings </small>
+            </RouterLink>
+
+            <hr class="dark:border-gray-700/60" />
+
+            <RouterLink to="Profile"
+                class="flex h-16 w-16 flex-col items-center justify-center gap-1 text-fuchsia-900 dark:text-gray-400">
+                <!-- HeroIcon - Home Modern -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+                </svg>
+
+                <small className="text-xs font-medium">Home</small>
+            </RouterLink>
+        </nav>
+
+        <nav
+            v-else class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
+            <RouterLink to="login"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                <!-- HeroIcon - User -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 shrink-0">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+
+                <small class="text-center text-xs font-medium"> Login </small>
+            </RouterLink>
+
+            <hr class="dark:border-gray-700/60" />
+
+            <a href="/Signup"
+                class="flex h-16 w-16 flex-col items-center justify-center gap-1 text-fuchsia-900 dark:text-gray-400">
+                <!-- HeroIcon - Home Modern -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+                </svg>
+
+                <small className="text-xs font-medium">Signup</small>
+            </a>
+        </nav>
+
+
+        <RouterView/>
+
+  
 
     
 
@@ -58,9 +101,23 @@
       <p class="text-center py-5">
         Crafted with ❤️ Tech Agents
         <span class="font-black"
-          ><Link href="https://twitter.com/sahilnetic">@sahilnetic</Link></span
+          ><Link href="https://twitter.com/">hi</Link></span
         >
       </p>
     </footer>
-  </div>
+ 
 </template>
+
+<script>
+    export default {
+        data() {
+            isLoggedIn : false
+        },
+        created(){
+            if(localStorage.getItem('token')){
+                this.isLoggedIn=true
+            }
+        },
+        
+    }
+</script>
