375e6d85fffef1828de166b4c589bdac9a231154
msinghkarki <mskarki01@gmail.com>
2023-10-29 04:30:42 +0530

    added logo to the launch page

--- pal_frontend/src/views/AppView.vue
+++ pal_frontend/src/views/AppView.vue
@@ -3,7 +3,7 @@
         <div class="sticky top-0 z-50">
             <div>
                     <div class="w-full lg:w-30/6 xl:w-full h-full flex items-center px-4">
-                        <img class="rounded-lg bg-white w-32" src="../assets/main logo.png" alt="addify logo" />
+                        <img class="rounded-lg bg-none w-32" src="../assets/main logo.png" alt="addify logo" style="width: 180px; height: 150px;" />
                     </div>
             </div>
         </div>
@@ -11,10 +11,12 @@
         
             <div class="relative z-10 max-w-screen-xl px-4 pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                 <div class="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-                    <h2 class="text-4xl font-bold text-center sm:text-5xl">
+                    <h2 class="text-4xl font-bold text-center bg-blue-500 sm:text-5xl">
                         Stay Linked, Stay Strong:
+                        </h2>
+                        <h2 class="text-4xl font-bold text-center sm:text-5xl">
                         <span class="hidden sm:block text-center text-4xl">Nurturing Connections Every Day.</span>
-                        <span class="hidden sm:block text-center mt-3 text-4xl">We lose our sleep</span>
+                        <span class="hidden sm:block text-center mt-5 text-4xl">We lose our sleep</span>
                         <span class="hidden sm:block text-center text-4xl">so you don't</span>
                         <span class="hidden sm:block text-center text-4xl">loose touch with your</span>
                         <span class="hidden sm:block text-center text-4xl">Friends and Connections...</span>
@@ -32,6 +34,5 @@
             <div class="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full">
                 <img class="w-2/3" src="../assets/friends.png" />
             </div>
-        
     </div>
 </template>
