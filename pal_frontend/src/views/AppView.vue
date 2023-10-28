6139f27f23883fccdab4a738bf76540fb5ca6c5f
msinghkarki <mskarki01@gmail.com>
2023-10-29 03:59:02 +0530

    intro to webapp

--- pal_frontend/src/views/AppView.vue
+++ pal_frontend/src/views/AppView.vue
@@ -1,39 +1,37 @@
 <template>
-    <div>
+    <div class="bg-[url(../assets/img4.jpg)] bg-center">
         <div class="sticky top-0 z-50">
             <div>
-                <div class="w-full bg-black opacity-80 h-20 flex justify-between">
                     <div class="w-full lg:w-30/6 xl:w-full h-full flex items-center px-4">
-                        <img class="rounded-lg bg-white w-32" src="../assets/logo.png" alt="addify logo" />
+                        <img class="rounded-lg bg-white w-32" src="../assets/main logo.png" alt="addify logo" />
                     </div>
-
-
-                </div>
             </div>
         </div>
-
-        <div class="text-7xl text-center">Pal app</div>
-
-        <aside class="relative h-screen r:\01 - Projects\pal\pal_frontend\src\views\SignupView.vue r:\01 - Projects\pal\pal_frontend\src\views\LoginView.vue r:\01 - Projects\pal\pal_frontend\src\views\LogoutView.vueoverflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+        <div class="text-7xl font-bold text-center">Pal App</div>
+        
             <div class="relative z-10 max-w-screen-xl px-4 pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                 <div class="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-                    <h2 class="text-4xl font-bold sm:text-5xl">
+                    <h2 class="text-4xl font-bold text-center sm:text-5xl">
                         Stay Linked, Stay Strong:
-                        <span class="hidden sm:block text-4xl">Nurturing Connections Every Day.</span>
+                        <span class="hidden sm:block text-center text-4xl">Nurturing Connections Every Day.</span>
+                        <span class="hidden sm:block text-center mt-3 text-4xl">We lose our sleep</span>
+                        <span class="hidden sm:block text-center text-4xl">so you don't</span>
+                        <span class="hidden sm:block text-center text-4xl">loose touch with your</span>
+                        <span class="hidden sm:block text-center text-4xl">Friends and Connections...</span>
                     </h2>
-
-                    <a class="inline-flex text-white items-center px-6 py-3 font-medium bg-rose-500 rounded-lg hover:opacity-75"
-                        href="">
-
-
-                        &nbsp; Register
+                    <div class="font-bold text-2xl text-center">Do checkout our Github Repo</div>
+                    <div class="flex justify-center">
+                        <a class="inline-flex justify-center text-white items-center px-6 py-3 font-medium bg-rose-500 rounded-lg hover:opacity-75"
+                        href="https://github.com/grow-xd/pal">
+                        &nbsp; Github
                     </a>
+                    </div>
                 </div>
             </div>
 
             <div class="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full">
                 <img class="w-2/3" src="../assets/friends.png" />
             </div>
-        </aside>
+        
     </div>
 </template>
